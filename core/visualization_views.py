bd81e7addce02e2bb684294da7de250bf1f9ede5
Gallium Wang <galliumwang199@gmail.com>
2020-11-29 15:00:17 +0800

    minor update for marked item view migration

---
+++ core/visualization_views.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+import stripe
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.views.generic import ListView, DetailView, View
+
+from .forms import CheckoutForm, CouponForm, RefundForm, PaymentForm
+from .models import *
+
+import csv
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def marked_item_net_view(request):
+
+    return render(request, "marked_item_net_view.html")
--- phaseReport.md
+++ phaseReport.md
@@ -1,3 +1,8 @@
 1. 多线程GCN数据生成
-2. add load gif
-3. add rating icon
+2. minor UI improvement
+3. add load gif
+4. add rating icon
+5. add marked item view
+6. add sentence analysis(in development)
+7. docs create
+8. Visualization cache
